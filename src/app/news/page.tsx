c445c1e9aade3091e2902e758b0d72f4bdef3744
IanCodes <ecleoceasar1@gmail.com>
2024-10-07 05:48:33 +0800

    adding active links

---
+++ src/app/news/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NewsPage() {
+  return (
+    <div>news page</div>
+  )
+}
+
+export default NewsPage
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,15 +6,20 @@ import { Button } from './ui/button'
 import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger } from './ui/navigation-menu'
 import { Switch } from "@/components/ui/switch"
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import { usePathname } from 'next/navigation'
 
 const Navbar = () => {
+
+
+    const pathname = usePathname()
+
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
     const toggleMobileMenu = () => {
         setIsMobileMenuOpen(!isMobileMenuOpen);
     }
 
   return (
-    <header className='py-4 bg-white text-gray-900 transition-colors duration-300 shadow-md'>
+    <header className='py-4 bg-blue-800 text-white transition-colors duration-300 shadow-md'>
         <nav className='max-w-7xl mx-auto flex justify-between items-center px-4 sm:px-6 lg:px-8'>
             {/* logo */}
             <div>
@@ -26,25 +31,25 @@ const Navbar = () => {
             <NavigationMenu className='hidden lg:flex'>
                 <NavigationMenuList className='flex space-x-7'>
                     <NavigationMenuItem>
-                        <NavigationMenuLink href='/news' className='hover: text-gray-600'>
+                        <NavigationMenuLink href='/news' className={`${pathname === '/news' ? 'text-green-500 font-semibold' : ''} hover: text-white`}>
                         News
                         </NavigationMenuLink>
                     </NavigationMenuItem>
 
                     <NavigationMenuItem>
                         
-                        <NavigationMenuTrigger className='dark:bg-gray-900 dark:text-white'>
+                        <NavigationMenuTrigger className='text-white bg-blue-800'>
                             Services
                         </NavigationMenuTrigger>
 
                         <NavigationMenuContent>
                             <NavigationMenuLink>
-                             <ul className='text-gray-500 shadow-md rounded-md py-4 px-5 space-x-2'>
+                             <ul className='text-gray-900 shadow-md rounded-md py-5 px-6 space-x-1'>
                                 <li>
                                     <NavigationMenuLink href='/services/web-development' className='hover: text-gray-600'>Web Development</NavigationMenuLink>
                                 </li>
                                 <li>
-                                    <NavigationMenuLink href='/services/app' className='hover: text-gray-600'>Mobile Apps</NavigationMenuLink>
+                                    <NavigationMenuLink href='/services/app' className='hover: text-gray-600'>Mobile App</NavigationMenuLink>
                                 </li>
                                 <li>
                                     <NavigationMenuLink href='/services/seo' className='hover: text-gray-600'>SEO</NavigationMenuLink>
@@ -56,13 +61,13 @@ const Navbar = () => {
                     </NavigationMenuItem>
 
                     <NavigationMenuItem>
-                        <NavigationMenuLink href='/contact' className='hover: text-gray-600'>
+                        <NavigationMenuLink href='/contact' className={`${pathname === '/contact' ? 'text-green-500 font-semibold' : ''} hover: text-white`}>
                         Contact
                         </NavigationMenuLink>
                     </NavigationMenuItem>
 
                     <NavigationMenuItem>
-                        <NavigationMenuLink href='/about' className='hover: text-gray-600'>
+                        <NavigationMenuLink href='/about' className={`${pathname === '/about' ? 'text-green-500 font-semibold' : ''} hover: text-white`}>
                         About
                         </NavigationMenuLink>
                     </NavigationMenuItem>
@@ -104,4 +109,4 @@ export default Navbar
     2. useState or useEffect
 */
 
-// last : https://youtu.be/RJ0lkR3wX80?t=3206
+// last : https://youtu.be/RJ0lkR3wX80?t=4046
